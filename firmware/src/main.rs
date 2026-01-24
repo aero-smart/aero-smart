9d397af047d822157d7b2148d7e9875f3aae4dd3
Ethan Goh <7086cmd@gmail.com>
2026-01-24 23:30:27 +0800

    refactor(firmware): send message should not block the mutex

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -25,7 +25,17 @@ use aerosmart_shared::serial::SerialMessage;
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::{
-    bind_interrupts, exti::ExtiInput, gpio::{Level, Output, Pull, Speed}, i2c::{self, I2c}, mode::Async, peripherals::IWDG1, spi::{self, Spi}, time::Hertz, timer::simple_pwm::{PwmPin, SimplePwm}, usart::{Uart, UartRx, UartTx}, wdg::IndependentWatchdog
+    bind_interrupts,
+    exti::ExtiInput,
+    gpio::{Level, Output, Pull, Speed},
+    i2c::{self, I2c},
+    mode::Async,
+    peripherals::IWDG1,
+    spi::{self, Spi},
+    time::Hertz,
+    timer::simple_pwm::{PwmPin, SimplePwm},
+    usart::{Uart, UartRx, UartTx},
+    wdg::IndependentWatchdog,
 };
 use embassy_time::{Duration, Timer};
 use smart_leds::RGB8;
@@ -135,7 +145,7 @@ async fn main(spawner: Spawner) {
     let sensors = Airspeed::new(i2c);
     let edf = EdfDshot::new(edf_pwm, p.DMA1_CH5);
     let pid = AirspeedControl::new(1.0, 0.1, 0.05);
-    let ahrs = MadgwickAhrs::new(4_000.0, 0.033);
+    let ahrs = MadgwickAhrs::new(1_000.0, 0.033);
 
     wdt.pet();
 
@@ -418,7 +428,7 @@ async fn imu_fft_task() {
             let mut imu_data = imu_buffer[axis];
             let fft_output = compute_motion_fft(&mut imu_data);
             let vibration_metrics =
-                crate::algorithms::motion_fft::analyze_vibration(&fft_output, 4000.0);
+                crate::algorithms::motion_fft::analyze_vibration(&fft_output, 1000.0);
             defmt::info!(
                 "IMU Axis {} Vibration Metrics: RMS Vibration: {} | Dominant Frequency: {} Hz | Peak Magnitude: {}",
                 axis,
@@ -517,74 +527,85 @@ async fn serial_uart_tx_task(mut tx: UartTx<'static, Async>) {
     loop {
         Timer::after(Duration::from_millis(100)).await;
         counter += 1;
-
-        {
-            use aerosmart_shared::serial::*;
+        use aerosmart_shared::serial::*;
+        let (airspeed, imu, quat, vibration, baro, lidar) = {
             let state = GLOBAL_STATE.lock().await;
+            let last_idx =
+                (state.imu_head + state.imu_buffer[0].len() - 1) % state.imu_buffer[0].len();
 
-            // Send airspeed @ 10 Hz
-            let airspeed_message = SerialMessage::PitotAirspeedData(PitotAirspeedData {
-                splitter_left: 0f32,
-                splitter_right: 0f32,
-                static_port: state.airspeed_meters_per_second,
+            let imu_data: [f32; 3] = core::array::from_fn(|i| state.imu_buffer[i][last_idx]);
+
+            (
+                state.airspeed_meters_per_second,
+                imu_data,
+                state.quaternion,
+                state.vibration_metrics,
+                state.barometer_data,
+                state.lidar_data,
+            )
+        };
+        // Send airspeed @ 10 Hz
+        let airspeed_message = SerialMessage::PitotAirspeedData(PitotAirspeedData {
+            splitter_left: 0f32,
+            splitter_right: 0f32,
+            static_port: airspeed,
+        });
+        let (buffer, len) = send_message(airspeed_message).await;
+        tx.write(&buffer[..len]).await.ok();
+
+        // Send IMU quaternion @ 20 Hz
+
+        if counter % 2 == 0
+            && let Some(quat) = quat
+        {
+            let imu_message = SerialMessage::ImuData(ImuData {
+                accel_z: imu[0],
+                gyro_x: imu[1],
+                gyro_y: imu[2],
+                quad_w: quat.w,
+                quad_i: quat.i,
+                quad_j: quat.j,
+                quad_k: quat.k,
             });
-            let (buffer, len) = send_message(airspeed_message).await;
+            let (buffer, len) = send_message(imu_message).await;
             tx.write(&buffer[..len]).await.ok();
+        }
 
-            // Send IMU quaternion @ 20 Hz
+        // Send vibration metrics @ 1 Hz
+        if counter % 10 == 0
+            && let Some(vibration_metrics) = vibration
+        {
+            let vibration_message = SerialMessage::ImuVibrationMetrics {
+                accel_z: vibration_metrics[0].into(),
+                gyro_x: vibration_metrics[1].into(),
+                gyro_y: vibration_metrics[2].into(),
+            };
+            let (buffer, len) = send_message(vibration_message).await;
+            tx.write(&buffer[..len]).await.ok();
+        }
 
-            if counter % 2 == 0
-                && let Some(quat) = state.quaternion
-            {
-                let imu_message = SerialMessage::ImuData(ImuData {
-                    accel_z: state.imu_buffer[0][(state.imu_head + 1023) % 1024],
-                    gyro_x: state.imu_buffer[1][(state.imu_head + 1023) % 1024],
-                    gyro_y: state.imu_buffer[2][(state.imu_head + 1023) % 1024],
-                    quad_w: quat.w,
-                    quad_i: quat.i,
-                    quad_j: quat.j,
-                    quad_k: quat.k,
-                });
-                let (buffer, len) = send_message(imu_message).await;
-                tx.write(&buffer[..len]).await.ok();
-            }
+        if counter % 10 == 0
+            && let Some(baro_data) = baro
+        {
+            // Send barometer data @ 1 Hz
 
-            // Send vibration metrics @ 1 Hz
-            if counter % 10 == 0
-                && let Some(vibration_metrics) = state.vibration_metrics
-            {
-                let vibration_message = SerialMessage::ImuVibrationMetrics {
-                    accel_z: vibration_metrics[0].into(),
-                    gyro_x: vibration_metrics[1].into(),
-                    gyro_y: vibration_metrics[2].into(),
-                };
-                let (buffer, len) = send_message(vibration_message).await;
-                tx.write(&buffer[..len]).await.ok();
-            }
+            let baro_message = SerialMessage::BarometerData(baro_data);
+            let (buffer, len) = send_message(baro_message).await;
+            tx.write(&buffer[..len]).await.ok();
+        }
 
-            if counter % 10 == 0
-                && let Some(baro_data) = &state.barometer_data
-            {
-                // Send barometer data @ 1 Hz
+        if counter % 2 == 0
+            && let Some(lidar_data) = lidar
+        {
+            // Send LIDAR data @ 5 Hz
 
-                let baro_message = SerialMessage::BarometerData(*baro_data);
-                let (buffer, len) = send_message(baro_message).await;
-                tx.write(&buffer[..len]).await.ok();
-            }
+            let lidar_message: SerialMessage = SerialMessage::LidarData(lidar_data);
+            let (buffer, len) = send_message(lidar_message).await;
+            tx.write(&buffer[..len]).await.ok();
+        }
 
-            if counter % 2 == 0
-                && let Some(lidar_data) = &state.lidar_data
-            {
-                // Send LIDAR data @ 5 Hz
-
-                let lidar_message: SerialMessage = SerialMessage::LidarData(*lidar_data);
-                let (buffer, len) = send_message(lidar_message).await;
-                tx.write(&buffer[..len]).await.ok();
-            }
-
-            if counter >= 100 {
-                counter = 0;
-            }
+        if counter >= 100 {
+            counter = 0;
         }
     }
 }
